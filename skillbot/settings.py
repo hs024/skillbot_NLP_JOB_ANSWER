9ed983d046664f53dc013156f529f5c64aeb9f35
himanshusharma <hs0242262@gmail.com>
2025-06-16 19:35:43 +0530

    live

--- skillbot/settings.py
+++ skillbot/settings.py
@@ -58,6 +58,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    'https://nlp-based-job-answer.vercel.app/'
 ]
 
 ROOT_URLCONF = 'skillbot.urls'
