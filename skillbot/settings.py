ce6250056f244880113cffa6af747d80161b29aa
himanshusharma <hs0242262@gmail.com>
2025-06-16 19:41:38 +0530

    safe-live

--- skillbot/settings.py
+++ skillbot/settings.py
@@ -54,11 +54,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
     'https://nlp-based-job-answer.vercel.app/'
+    
 ]
 
 ROOT_URLCONF = 'skillbot.urls'
